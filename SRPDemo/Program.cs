b35ca4c86e13037eb75994d49b72096a3ff77f86
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-21 11:23:55 +0100

    Add project files.

---
+++ SRPDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SRPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application!");
+
+            Person user = new Person();
+
+            Console.WriteLine("What is your first name: ");
+            user.FirstName = Console.ReadLine();
+
+
+            Console.WriteLine("What is your last name: ");
+            user.LastName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("You did not give us a valid first name!");
+                user.FirstName = Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You did not give us a valid last name!");
+                user.LastName = Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
