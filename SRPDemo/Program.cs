c82d67e0a8cb2064f122681622268b86d1ea3625
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-21 11:37:01 +0100

    start up file

--- SRPDemo/Program.cs
+++ SRPDemo/Program.cs
@@ -8,15 +8,17 @@ namespace SRPDemo
         {
             Console.WriteLine("Welcome to my application!");
 
+            //Ask for user information
             Person user = new Person();
 
             Console.WriteLine("What is your first name: ");
             user.FirstName = Console.ReadLine();
-
-
+            
             Console.WriteLine("What is your last name: ");
             user.LastName = Console.ReadLine();
 
+
+            //check for valid detail
             if (string.IsNullOrWhiteSpace(user.FirstName))
             {
                 Console.WriteLine("You did not give us a valid first name!");
@@ -30,6 +32,10 @@ namespace SRPDemo
                 user.LastName = Console.ReadLine();
                 return;
             }
+
+            //create user
+            Console.WriteLine($"Your username is {user.FirstName.Substring(0, 1) }{ user.LastName }");
+            Console.ReadLine();
         }
     }
 }
