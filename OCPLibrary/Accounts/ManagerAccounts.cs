8f7178c9e43ab8558646950dc6a49f9740577a7d
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-21 17:30:59 +0100

    update the code for email format change

--- OCPLibrary/Accounts/ManagerAccounts.cs
+++ OCPLibrary/Accounts/ManagerAccounts.cs
@@ -6,7 +6,18 @@ namespace OCPLibrary.Accounts
     {
         public EmployeeModel Create(IApplicantModel person)
         {
-            var employee = EmployeeCapture.CreateEmployee(person);
+            //var employee = EmployeeCapture.CreateEmployee(person);
+
+            //The above code can not be use here before the email format is different.
+            //Someone need to be careful when using the CreateEmployee class
+
+            EmployeeModel employee = new EmployeeModel
+            {
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                EmailAddress = $"{ person.FirstName.Substring(0, 3) }{person.LastName}@heyzed.com"
+            };
+
             employee.IsManager = true;
             return employee;
         }
