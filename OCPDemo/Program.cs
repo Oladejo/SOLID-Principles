5ff4ab94ff64e623f2ed72f48bbe3d87928a11fa
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-21 15:29:57 +0100

    starting file for Open and closed principles

---
+++ OCPDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using OCPLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace OCPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> applicants = new List<PersonModel>
+            {
+                new PersonModel { FirstName = "Azeez", LastName = "Tunde"},
+                new PersonModel { FirstName = "Ade", LastName = "Saka"},
+                new PersonModel { FirstName = "Maryam", LastName = "Fareedah"}
+            };
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            Accounts accountProcessor = new Accounts();
+
+            foreach(var person in applicants)
+            {
+                employees.Add(accountProcessor.Create(person));
+            }
+
+            foreach(var emp in employees)
+            {
+                Console.WriteLine($"{emp.FirstName} { emp.LastName } : {emp.EmailAddress}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OCPLibrary/Accounts.cs
@@ -0,0 +1,17 @@
+﻿namespace OCPLibrary
+{
+    public class Accounts
+    {
+        public EmployeeModel Create(PersonModel person)
+        {
+            EmployeeModel employee = new EmployeeModel
+            {
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                EmailAddress = $"{ person.FirstName.Substring(0, 1) }{person.LastName}@heyzed.com"
+            };
+
+            return employee;
+        }
+    }
+}
