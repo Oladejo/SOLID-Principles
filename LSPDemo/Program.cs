b1e86d2b519584057c42bd1359dadeeb889021e6
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-02-27 14:13:09 +0100

    add Liskov Substitution pattern to the project

---
+++ LSPDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LSPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
