c21c6c6a72ad89e21b3b150c61323eb9f1853732
Oladejo <bab.oladejo@student.oauife.edu.ng>
2020-03-24 20:46:38 +0100

    start project to handle ISP principle

---
+++ ISPDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ISPDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The interface-segregation principle (ISP) states that no 
+            //client should be forced to depend on methods it does not use
+            Console.WriteLine("Interface Segregation Principle!");
+        }
+    }
+}
---
+++ ISPLibrary/DVD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ISPLibrary
+{
+    public class DVD : ILibraryItem
+    {
+        public string LibraryId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; } = "";
+        public int CheckOutDurationInDays { get; set; } = 14;
+        public int Pages { get; set; } = -1;
+        public string Borrower { get; set; }
+        public DateTime BorrowDate { get ; set ; }
+        public List<string> Actors { get; set; }
+        public int RuntimeInMinutes { get; set; }
+
+        public void CheckIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckOut(string borrower)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDueDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
